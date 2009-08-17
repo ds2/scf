75e9bdd1129fbc9992fb703cc36d19bf05711011
Lexxy <lexxy23@gmail.com>
2009-08-17 05:55:10 +0200

    - first idea of the appengine version but not yet installable

---
+++ plugins/appengine-website/src/main/java/com/googlecode/socofo/appengine/StartupListener.java
@@ -0,0 +1,53 @@
+/**
+ * SoCoFo Source Code Formatter
+ * Copyright (C) 2009 Dirk Strauss <lexxy23@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * 
+ */
+package com.googlecode.socofo.appengine;
+
+import java.util.logging.Logger;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @author kaeto23
+ * 
+ */
+public class StartupListener implements ServletContextListener {
+	private static final Logger LOG = Logger.getLogger(StartupListener.class
+			.getName());
+
+	/**
+	 * 
+	 */
+	public StartupListener() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		LOG.info("Context destroyed");
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		LOG.info("Context initialized");
+	}
+
+}
