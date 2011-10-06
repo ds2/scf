3d3aa9bf0c16eb1547ea67527cc7e8e9e0e39eb6
dstrauss <dstrauss@ds-2.de>
2011-10-06 15:14:49 +0200

    SCF-000: some more changes for the rt
    Signed-off-by: dstrauss <dstrauss@ds-2.de>

--- main/runtime/src/main/java/com/googlecode/socofo/runtime/impl/DefRuntime.java
+++ main/runtime/src/main/java/com/googlecode/socofo/runtime/impl/DefRuntime.java
@@ -28,11 +28,12 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import com.googlecode.socofo.common.api.MainRuntime;
 import com.googlecode.socofo.core.api.Scheduler;
 import com.googlecode.socofo.core.api.SourceTypes;
@@ -46,329 +47,326 @@ import com.googlecode.socofo.core.api.TranslationJob;
  */
 @Singleton
 public class DefRuntime implements MainRuntime {
-
-	/**
-	 * A logger.
-	 */
-	private static final Logger log = LoggerFactory.getLogger(DefRuntime.class);
-
-	/**
-	 * the console to write log information to.
-	 */
-	private PrintWriter console = null;
-	/**
-	 * The scheduler.
-	 */
-	@Inject
-	private Scheduler scheduler = null;
-	/**
-	 * The base directory that contains the source code files.
-	 */
-	private File baseDir = null;
-	/**
-	 * The target directory to write the transformed source code to.
-	 */
-	private File targetDir = null;
-
-	/**
-	 * Flag to indicate that the help screen should be displayed.
-	 */
-	private boolean showHelp = false;
-	/**
-	 * The url to the transformation rules.
-	 */
-	private URL rulesUrl;
-	/**
-	 * The list of source types to scan for.
-	 */
-	private List<SourceTypes> types = null;
-	/**
-	 * Some source filters.
-	 */
-	private List<String> sourceFilters = null;
-
-	/**
-	 * Inits the runtime.
-	 */
-	public DefRuntime() {
-		final Console c = System.console();
-		if (c == null) {
-			console = new PrintWriter(System.out);
-		} else {
-			console = c.writer();
-		}
-		types = new ArrayList<SourceTypes>();
-		sourceFilters = new ArrayList<String>();
-	}
-
-	/**
-	 * Prints the help information.
-	 */
-	private void printHelp() {
-		console.println("SoCoFo Source Code Formatter");
-		console.println("Copyright (C) 2009 Dirk Strauss <lexxy23@gmail.com>");
-
-		console.println();
-		console.println("Parameters:");
-		console.println(PARAM_RULESURL
-				+ " = sets the url where the formatter rules can be found ("
-				+ PARAM_RULESURL
-				+ "=http://test.local/test/formatterRules.xml)");
-		console.println(PARAM_BASEDIR
-				+ " = the base directory to scan for source files");
-		console
-				.println(PARAM_TARGETDIR
-						+ " = the directory to write the results to; default is the base directory");
-		console
-				.println(PARAM_TYPES
-						+ " = a list of types to transform; Supported types currently: xml");
-	}
-
-	/**
-	 * Sets the scheduler for the tests.
-	 * 
-	 * @param instance
-	 *            an instance of a scheduler
-	 */
-	protected void setTestScheduler(final Scheduler instance) {
-		scheduler = instance;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public int execute() {
-		int rc = 0;
-		if (showHelp) {
-			printHelp();
-			return RC_SHOWHELP;
-		}
-		if (scheduler == null) {
-			log.error("No scheduler set!");
-			return RC_NOSCHEDULER;
-		}
-		if (rulesUrl == null) {
-			log.error("No rulesUrl given!");
-			return RC_NORULES;
-		}
-		if (types.size() <= 0) {
-			log.error("No types given to scan for!");
-			return RC_NOTYPES;
-		}
-		final Thread currentThread = Thread.currentThread();
-		scheduler.addWaiterThreads(currentThread);
-		List<TranslationJob> jobs = null;
-		scheduler.setRules(rulesUrl);
-		if (scheduler.getRuleset() == null) {
-			return RC_NORULES;
-		}
-		baseDir = getBaseDirectory();
-		log.info("Using base directory " + baseDir);
-		log.info("Using target directory " + targetDir);
-		jobs = scheduler.createLocalJobs(baseDir, targetDir, types);
-		if (jobs.size() <= 0) {
-			log.warn("No source files found!");
-			return RC_NOSOURCES;
-		}
-		scheduler.addJobs(jobs);
-		log.debug("Starting scheduler");
-		scheduler.startScheduler();
-		while (scheduler.getActiveJobsCount() > 0) {
-			try {
-				Thread.sleep(250);
-			} catch (final InterruptedException e) {
-				log
-						.debug(
-								"Interrupted while sleeping, but this is not really critical",
-								e);
-			}
-		}
-		final List<String> errorMessages = scheduler.getErrorMessages();
-		if (!errorMessages.isEmpty()) {
-			rc = RC_TRANSFORM;
-			console.println("Some errors occurred:");
-			for (String msg : errorMessages) {
-				console.println("*) " + msg);
-			}
-		}
-		console.println("finished");
-		log.debug("exiting: {}", rc);
-		return rc;
-	}
-
-	/**
-	 * Convenient method for parseParams and execute, in one method.
-	 * 
-	 * @param args
-	 *            the parameters.
-	 * @return the result of the execution.
-	 */
-	public int execute(final String... args) {
-		parseParams(args);
-		return execute();
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public File getBaseDirectory() {
-		if (baseDir == null) {
-			baseDir = new File("");
-		}
-		return baseDir;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public File getTargetDirectory() {
-		return targetDir;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void parseParams(final String... args) {
-		log.debug("entering: {}", args);
-		if (args != null && args.length > 0) {
-			for (String arg : args) {
-				log.debug("current arg is {}", arg);
-				if (arg == null || arg.length() <= 0) {
-					continue;
-				}
-				checkParam(arg);
-			}
-		} else {
-			showHelp = true;
-		}
-		log.debug("exiting");
-	}
-
-	/**
-	 * Checks a given param if it is a known parameter. This method fills the
-	 * internal fields.
-	 * 
-	 * @param arg
-	 *            the parameter to check
-	 */
-	private void checkParam(final String arg) {
-		if (arg.startsWith(PARAM_BASEDIR) && baseDir == null) {
-			final String argSeq = arg.substring(PARAM_BASEDIR.length() + 1);
-			baseDir = new File(argSeq);
-			if (!baseDir.exists()) {
-				log.warn("Directory {} does not exist!", baseDir);
-			}
-		} else if (arg.startsWith(PARAM_RULESURL) && rulesUrl == null) {
-			try {
-				rulesUrl = new URL(arg.substring(PARAM_RULESURL.length() + 1));
-			} catch (final MalformedURLException e) {
-				log.debug(e.getLocalizedMessage(), e);
-			}
-		} else if (arg.startsWith(PARAM_TARGETDIR) && targetDir == null) {
-			final String argSeq = arg.substring(PARAM_TARGETDIR.length() + 1);
-			targetDir = new File(argSeq);
-		} else if (arg.startsWith(PARAM_HELP)) {
-			showHelp = true;
-		} else if (arg.startsWith(PARAM_TYPES)) {
-			final String argSeq = arg.substring(PARAM_TYPES.length() + 1);
-			final String[] typesIds = argSeq.split(",");
-			for (String typeId : typesIds) {
-				if (typeId != null && typeId.equalsIgnoreCase("xml")) {
-					types.add(SourceTypes.XML);
-				}
-			}
-		}
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public boolean showHelpScreen() {
-		return showHelp;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void resetSettings() {
-		baseDir = null;
-		targetDir = null;
-		showHelp = false;
-		rulesUrl = null;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public URL getRulesUrl() {
-		return rulesUrl;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void setBaseDirectory(final File srcDir) {
-		baseDir = srcDir;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void setTargetDirectory(final File t) {
-		this.targetDir = t;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void setRules(final URL r) {
-		this.rulesUrl = r;
-	}
-
-	/**
-	 * Returns the source types to work with.
-	 * 
-	 * @return the source types
-	 */
-	public List<SourceTypes> getTypes() {
-		return types;
-	}
-
-	@Override
-	public void applySourceFilters(List<String> filters) {
-		if (filters == null || filters.size() <= 0) {
-			return;
-		}
-		sourceFilters.addAll(filters);
-	}
-
-	@Override
-	public void setTypes(List<String> types) {
-		if (types == null || types.size() <= 0) {
-			return;
-		}
-		for (String t : types) {
-			t = t.trim().toLowerCase();
-			SourceTypes detectedType = SourceTypes.findByName(t);
-			if (detectedType == null) {
-				log
-						.warn(
-								"Could not find a type with the name {}! Continue types scan.",
-								t);
-				continue;
-			}
-			this.types.add(detectedType);
-		}
-	}
-
+    
+    /**
+     * A logger.
+     */
+    private static final Logger log = LoggerFactory.getLogger(DefRuntime.class);
+    
+    /**
+     * the console to write log information to.
+     */
+    private PrintWriter console = null;
+    /**
+     * The scheduler.
+     */
+    @Inject
+    private Scheduler scheduler = null;
+    /**
+     * The base directory that contains the source code files.
+     */
+    private File baseDir = null;
+    /**
+     * The target directory to write the transformed source code to.
+     */
+    private File targetDir = null;
+    
+    /**
+     * Flag to indicate that the help screen should be displayed.
+     */
+    private boolean showHelp = false;
+    /**
+     * The url to the transformation rules.
+     */
+    private URL rulesUrl;
+    /**
+     * The list of source types to scan for.
+     */
+    private List<SourceTypes> types = null;
+    /**
+     * Some source filters.
+     */
+    private List<String> sourceFilters = null;
+    
+    /**
+     * Inits the runtime.
+     */
+    public DefRuntime() {
+        final Console c = System.console();
+        if (c == null) {
+            console = new PrintWriter(System.out);
+        } else {
+            console = c.writer();
+        }
+        types = new ArrayList<SourceTypes>();
+        sourceFilters = new ArrayList<String>();
+    }
+    
+    /**
+     * Prints the help information.
+     */
+    private void printHelp() {
+        console.println("SoCoFo Source Code Formatter");
+        console.println("Copyright (C) 2009 Dirk Strauss <lexxy23@gmail.com>");
+        
+        console.println();
+        console.println("Parameters:");
+        console.println(PARAM_RULESURL
+            + " = sets the url where the formatter rules can be found ("
+            + PARAM_RULESURL + "=http://test.local/test/formatterRules.xml)");
+        console.println(PARAM_BASEDIR
+            + " = the base directory to scan for source files");
+        console
+            .println(PARAM_TARGETDIR
+                + " = the directory to write the results to; default is the base directory");
+        console
+            .println(PARAM_TYPES
+                + " = a list of types to transform; Supported types currently: xml");
+    }
+    
+    /**
+     * Sets the scheduler for the tests.
+     * 
+     * @param instance
+     *            an instance of a scheduler
+     */
+    protected void setTestScheduler(final Scheduler instance) {
+        scheduler = instance;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int execute() {
+        int rc = 0;
+        if (showHelp) {
+            printHelp();
+            return RC_SHOWHELP;
+        }
+        if (scheduler == null) {
+            log.error("No scheduler set!");
+            return RC_NOSCHEDULER;
+        }
+        if (rulesUrl == null) {
+            log.error("No rulesUrl given!");
+            return RC_NORULES;
+        }
+        if (types.size() <= 0) {
+            log.error("No types given to scan for!");
+            return RC_NOTYPES;
+        }
+        final Thread currentThread = Thread.currentThread();
+        scheduler.addWaiterThreads(currentThread);
+        List<TranslationJob> jobs = null;
+        scheduler.setRules(rulesUrl);
+        if (scheduler.getRuleset() == null) {
+            return RC_NORULES;
+        }
+        baseDir = getBaseDirectory();
+        log.info("Using base directory " + baseDir);
+        log.info("Using target directory " + targetDir);
+        jobs = scheduler.createLocalJobs(baseDir, targetDir, types);
+        if (jobs.size() <= 0) {
+            log.warn("No source files found!");
+            return RC_NOSOURCES;
+        }
+        scheduler.addJobs(jobs);
+        log.debug("Starting scheduler");
+        scheduler.startScheduler();
+        while (scheduler.getActiveJobsCount() > 0) {
+            try {
+                Thread.sleep(250);
+            } catch (final InterruptedException e) {
+                log.debug(
+                    "Interrupted while sleeping, but this is not really critical",
+                    e);
+            }
+        }
+        final List<String> errorMessages = scheduler.getErrorMessages();
+        if (!errorMessages.isEmpty()) {
+            rc = RC_TRANSFORM;
+            console.println("Some errors occurred:");
+            for (String msg : errorMessages) {
+                console.println("*) " + msg);
+            }
+        }
+        console.println("finished");
+        log.debug("exiting: {}", rc);
+        return rc;
+    }
+    
+    /**
+     * Convenient method for parseParams and execute, in one method.
+     * 
+     * @param args
+     *            the parameters.
+     * @return the result of the execution.
+     */
+    public int execute(final String... args) {
+        parseParams(args);
+        return execute();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public File getBaseDirectory() {
+        if (baseDir == null) {
+            baseDir = new File("");
+        }
+        return baseDir;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public File getTargetDirectory() {
+        return targetDir;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void parseParams(final String... args) {
+        log.debug("entering: {}", args);
+        if (args != null && args.length > 0) {
+            for (String arg : args) {
+                log.debug("current arg is {}", arg);
+                if (arg == null || arg.length() <= 0) {
+                    continue;
+                }
+                checkParam(arg);
+            }
+        } else {
+            showHelp = true;
+        }
+        log.debug("exiting");
+    }
+    
+    /**
+     * Checks a given param if it is a known parameter. This method fills the
+     * internal fields.
+     * 
+     * @param arg
+     *            the parameter to check
+     */
+    private void checkParam(final String arg) {
+        if (arg.startsWith(PARAM_BASEDIR) && baseDir == null) {
+            final String argSeq = arg.substring(PARAM_BASEDIR.length() + 1);
+            baseDir = new File(argSeq);
+            if (!baseDir.exists()) {
+                log.warn("Directory {} does not exist!", baseDir);
+            }
+        } else if (arg.startsWith(PARAM_RULESURL) && rulesUrl == null) {
+            try {
+                rulesUrl = new URL(arg.substring(PARAM_RULESURL.length() + 1));
+            } catch (final MalformedURLException e) {
+                log.debug(e.getLocalizedMessage(), e);
+            }
+        } else if (arg.startsWith(PARAM_TARGETDIR) && targetDir == null) {
+            final String argSeq = arg.substring(PARAM_TARGETDIR.length() + 1);
+            targetDir = new File(argSeq);
+        } else if (arg.startsWith(PARAM_HELP)) {
+            showHelp = true;
+        } else if (arg.startsWith(PARAM_TYPES)) {
+            final String argSeq = arg.substring(PARAM_TYPES.length() + 1);
+            final String[] typesIds = argSeq.split(",");
+            for (String typeId : typesIds) {
+                if (typeId != null && typeId.equalsIgnoreCase("xml")) {
+                    types.add(SourceTypes.XML);
+                }
+            }
+        }
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean showHelpScreen() {
+        return showHelp;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void resetSettings() {
+        baseDir = null;
+        targetDir = null;
+        showHelp = false;
+        rulesUrl = null;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public URL getRulesUrl() {
+        return rulesUrl;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setBaseDirectory(final File srcDir) {
+        baseDir = srcDir;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setTargetDirectory(final File t) {
+        this.targetDir = t;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setRules(final URL r) {
+        this.rulesUrl = r;
+    }
+    
+    /**
+     * Returns the source types to work with.
+     * 
+     * @return the source types
+     */
+    public List<SourceTypes> getTypes() {
+        return types;
+    }
+    
+    @Override
+    public void applySourceFilters(List<String> filters) {
+        if (filters == null || filters.size() <= 0) {
+            return;
+        }
+        sourceFilters.addAll(filters);
+    }
+    
+    @Override
+    public void setTypes(List<String> types) {
+        if (types == null || types.size() <= 0) {
+            return;
+        }
+        for (String t : types) {
+            t = t.trim().toLowerCase();
+            SourceTypes detectedType = SourceTypes.findByName(t);
+            if (detectedType == null) {
+                log.warn(
+                    "Could not find a type with the name {}! Continue types scan.",
+                    t);
+                continue;
+            }
+            this.types.add(detectedType);
+        }
+    }
+    
 }
